3e39d424cd988ea5044852a1e80f7ca0b4de79b7
izrvnar <izrvnar@gmail.com>
2023-02-04 19:49:30 -0500

    added injectable and a constructor

---
+++ src/services/VideoGame.ts
@@ -0,0 +1,13 @@
+import { IContent } from "src/app/models/icontent";
+import { Injectable } from "@angular/core";
+import { contentList } from "src/data/mock-content";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class VideoGameService {
+    constructor() { 
+        
+    }
+}
