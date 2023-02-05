ce4dc2e91f4d058f167fde09136d4860eca2d620
izrvnar <izrvnar@gmail.com>
2023-02-04 20:21:13 -0500

    added the new servuce and retrieved the data from a new location

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IContent } from '../models/icontent';
+import { VideoGamesService } from '../services/video-games.service';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
+export class ContentListComponent implements OnInit {
   //define array of Icontent with 4 items
   evenBackgroundClass = 'even-background';
   oddBackgroundClass = 'odd-background'
+  contentList: IContent[];
   
 
-  constructor() {
+  constructor(private videoGamesService: VideoGamesService) {
+    this.contentList = [];
+   }
+
+  ngOnInit() {
+    this.videoGamesService.getVideoGames().subscribe((IContentArrayOfData: IContent[]) => {
+      this.contentList = IContentArrayOfData;
+    });
+  }
 
 }
-}
+
