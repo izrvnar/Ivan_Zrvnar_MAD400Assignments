cc14a1fcbde2596b0a18e1368b0f225e2625bd19
izrvnar <izrvnar@gmail.com>
2023-02-06 08:09:24 -0500

    added injection to show a single content item at the top of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { IContent } from './models/icontent';
+import { VideoGamesService } from './services/video-games.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'I_Zrvnar_FavouriteVideoGames';
-
+  contentItem: IContent | undefined;
+  constructor(@Inject(VideoGamesService) private videoGameService: VideoGamesService) { 
+    this.videoGameService.getVideoGameById(2).subscribe(content => this.contentItem = content);
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item.component';
+import { VideoGamesService } from './services/video-games.service';
 
 
 @NgModule({
@@ -15,7 +16,9 @@ import { ContentListItemComponent } from './content-list-item/content-list-item.
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    VideoGamesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
