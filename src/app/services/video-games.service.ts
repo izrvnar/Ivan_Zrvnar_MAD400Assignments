b542532b46b956a56e72c53127cc7d435bb9a093
izrvnar <izrvnar@gmail.com>
2023-02-06 07:31:58 -0500

    added CRUD methoids to the application

--- src/app/services/video-games.service.ts
+++ src/app/services/video-games.service.ts
@@ -7,11 +7,38 @@ import { IContent } from '../models/icontent';
   providedIn: 'root'
 })
 export class VideoGamesService {
+  private videoGames: IContent[] = contentList;
 
   constructor() { }
 
   getVideoGames(): Observable<IContent[]> {
     return of(contentList);
+  }
+
+  getVideoGameById(id: number): Observable<IContent> {
+    const videoGame = this.videoGames.find(content => content.id === id);
+    if (!videoGame) {
+      throw new Error(`Video game with id ${id} not found`);
+    }
+    return of(videoGame);
+  }
+
+
+  addVideoGame(videoGame: IContent): Observable<IContent[]> {
+    this.videoGames.push(videoGame);
+    return of(this.videoGames);
+  }
+
+  updateVideoGame(videoGame: IContent): Observable<IContent[]> {
+    const index = this.videoGames.findIndex(content => content.id === videoGame.id);
+    this.videoGames[index] = videoGame;
+    return of(this.videoGames);
+  }
+
+  removeVideoGame(id: number): Observable<IContent> {
+    const index = this.videoGames.findIndex(content => content.id === id);
+    const removedVideoGame = this.videoGames.splice(index, 1);
+    return of(removedVideoGame[0]);
   }
 
 }
