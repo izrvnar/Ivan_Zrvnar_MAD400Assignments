b30c62c4695f680c544e936901f79ef7d7d6f6a4
izrvnar <izrvnar@gmail.com>
2023-02-07 21:06:13 -0500

    added routes to the content detail view and redirect if they visit root

--- src/app/app--routing.module.ts
+++ src/app/app--routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentListComponent } from './content-list/content-list.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
 
 const routes: Routes = [
 
-  {path: "content",
-component: ContentListComponent,
-}
+  {path: "content", component: ContentListComponent}, 
+  {path: "content/id", component: ContentDetailComponent},
+  {path: "", redirectTo: "/content", pathMatch: "full"}
 ]
 
 
