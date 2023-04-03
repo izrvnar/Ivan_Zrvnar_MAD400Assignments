8c7bb4e44a91a024ad8b06b01fc2acea1ef2c0ef
izrvnar <izrvnar@gmail.com>
2023-04-03 19:03:20 -0400

    added ability for the user to add content to the list

--- src/app/modify-content/modify-content.component.ts
+++ src/app/modify-content/modify-content.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IContent } from '../models/icontent';
+import { VideoGamesService } from '../services/video-games.service';
 
 @Component({
   selector: 'app-modify-content',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./modify-content.component.scss']
 })
 export class ModifyContentComponent {
+  newContent: IContent = {
+    id: 0,
+    title: '',
+    author: '',
+    type: '',
+    description: '',
+    imgSrc: '',
+    tags: []
+  };
+  tags = '';
+  contentAdded = false;
+  contentAddedMessage = 'Content successfully added!';
 
+  constructor(private videoGamesService: VideoGamesService) {}
+
+  onSubmit(form: NgForm) {
+    this.newContent.tags = this.tags.split(',').map(tag => tag.trim());
+    this.videoGamesService.addVideoGame(this.newContent).subscribe(addedContent => {
+      this.contentAdded = true;
+      form.reset();
+      this.tags = '';
+      setTimeout(() => {
+        this.contentAdded = false;
+      }, 3000);
+    });
+  }
 }
